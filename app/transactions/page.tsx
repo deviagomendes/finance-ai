c5574a3abbed0b96cf440816479542b0541f4cb4
Iago Mendes <iagomendes152@gmail.com>
2025-01-20 17:10:26 -0300

    fix: changes before formating computer

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,20 @@
-const TransactionsPage = () => {
-  return ( <h1>Transactions</h1> );
-}
- 
+import { Button } from "../_components/ui/button";
+import { db } from "../_lib/prisma";
+import { ArrowDownUpIcon } from "lucide-react";
+
+const TransactionsPage = async () => {
+  const transactions = await db.transaction.findMany({});
+  return (
+    <div>
+      <div className="flex w-full items-center justify-between p-6">
+        <h1 className="text-2xl font-bold">Transações</h1>
+        <Button className="rounded-full">
+          Adicionar Transaçao
+          <ArrowDownUpIcon/>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
 export default TransactionsPage;
